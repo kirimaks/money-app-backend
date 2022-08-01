f3ded077d28628bf31220e8c001b5af6d59cf184
kirimaks <kirimaks@yahoo.com>
2022-08-01 13:59:11 +0000

    Config object validation.

--- src/schemas/environment.ts
+++ src/schemas/environment.ts
@@ -4,7 +4,7 @@ export const envConfigSchema = {
         ELASTIC_URL: {type: 'string'},
         ELASTIC_USER: {type: 'string'},
         ELASTIC_PASSWORD: {type: 'string'},
-        ACCOUNTS_INDEX_NAME: {type: 'string'},
+        ACCOUNTS_INDEX_NAME: {type: 'string', 'minLength': 1},
     },
     required: [
         'ELASTIC_URL', 'ELASTIC_USER', 'ELASTIC_PASSWORD', 'ACCOUNTS_INDEX_NAME'
