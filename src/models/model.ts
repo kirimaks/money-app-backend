009c90f6208717c438b120f52eb367f5800b16b6
kirimaks <kirimaks@yahoo.com>
2022-08-03 16:33:13 +0000

    Model response errors.

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,8 +1,29 @@
+function isModelResponse(catchedError:unknown): catchedError is ModelResponse {
+    return (
+        typeof catchedError === 'object' && catchedError != null && 'errorMessage' in catchedError
+    );
+}
+ 
+
 abstract class AbstractModel { 
-    abstract createDocument(document:unknown):Promise<ModelCreateDocResponse<unknown>>;
+    abstract createDocument(document:unknown):object;
+    abstract saveDocument(document:unknown):Promise<ModelCreateDocResponse<unknown>>;
     abstract removeDocument(docId:string):Promise<ModelDeleteDocResponse<unknown>>;
     abstract getDocument(docId:string):Promise<ModelSearchDocResponse<unknown>>;
     // abstract searchDocument(document:unknown):unknown[];
+
+   
+    getModelResponseError(catchedError:unknown):string {
+        if (catchedError instanceof Error) {
+            return catchedError.message;
+        }
+
+        if (isModelResponse(catchedError)) {
+            return catchedError.errorMessage;
+        }
+
+        return 'Cannot parse error message';
+    }
 }
 
 export {AbstractModel};
