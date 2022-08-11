98b07da09bbedbf005fe11354f76df76a12612c5
kirimaks <kirimaks@yahoo.com>
2022-08-11 14:21:04 +0000

    Session auth.

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,18 +1,22 @@
+import {validate as validateUUID} from 'uuid';
+import fp from 'fastify-plugin';
+
+import {getErrorMessage} from '../errors/tools';
+
 import type { 
     FastifyPluginAsync, ContextConfigDefault, FastifyInstance, FastifyReply,
     FastifyRequest, FastifySchema, preHandlerHookHandler 
 } from 'fastify';
 import type { RouteGenericInterface } from 'fastify/types/route';
-
 import type {FastifyAuthFunction} from '@fastify/auth';
+import type {Session} from '@fastify/secure-session';
 
-import fp from 'fastify-plugin';
 
 
 declare module 'fastify' {
     interface FastifyInstance<RawServer, RawRequest, RawReply, Logger, TypeProvider> {
         verifyJWTToken: (request:FastifyRequest, reply:FastifyReply) => void;
-        verifyUserAndPassword: (request:FastifyRequest, reply:FastifyReply) => void;
+        verifyUserSession: (request:FastifyRequest, reply:FastifyReply) => void;
         auth(
             functions: FastifyAuthFunction[],
             options?: {
@@ -21,7 +25,17 @@ declare module 'fastify' {
             }
         ): preHandlerHookHandler<RawServer, RawRequest, RawReply, RouteGenericInterface, ContextConfigDefault, FastifySchema, TypeProvider>;
     }
+
+    interface FastifyRequest {
+        session: Session;
+        user: UserSessionInfo;
+    }
 }
+
+const SESSION_AUTH_EXCLUDE = [
+    '/auth/login',
+    '/auth/signup'
+];
 
 interface AuthOptions {}
 
@@ -30,27 +44,44 @@ const authPlugin:FastifyPluginAsync<AuthOptions> = async (fastify:FastifyInstanc
     fastify.decorate('verifyJWTToken', async (_request:FastifyRequest, _reply:FastifyReply) => {
         fastify.log.debug('Calling verifyJWTToken');
 
-        if (Math.random() > 0.5) {
-            return true;
-        }
-
         throw Error('JWT auth failed');
     });
 
-    fastify.decorate('verifyUserAndPassword', async (_request:FastifyRequest, _reply:FastifyReply) => {
-        fastify.log.debug('Calling verifyUserAndPassword');
+    fastify.decorate('verifyUserSession', async (request:FastifyRequest, _reply:FastifyReply) => {
+        fastify.log.debug('Calling verifyUserSession');
 
-        if (Math.random() > 0.5) {
-            return true;
+        if (request.raw && request.raw.url) {
+            if (SESSION_AUTH_EXCLUDE.includes(request.raw.url)) {
+                return true;
+            }
         }
 
-        throw Error('User/password auth failed');
+        try {
+            const session = request.session.get('user');
+
+            if (session) {
+                /* TODO: validate session time */
+                if (validateUUID(session.user_id) && validateUUID(session.account_id)) {
+                    request.user = {
+                        user_id: session.user_id,
+                        account_id: session.account_id,
+                        anonymous: false,
+                    }
+                    return true;
+                }
+            }
+        } catch(error) {
+            const errorMessage = getErrorMessage(error);
+            fastify.log.error(`Cannot validate session: ${errorMessage}`);
+        }
+
+        throw Error('Invalid session');
     });
 
     fastify.register(require('@fastify/auth')).after(() => {
         fastify.addHook(
             'preHandler', 
-            fastify.auth([fastify.verifyJWTToken, fastify.verifyUserAndPassword])
+            fastify.auth([fastify.verifyJWTToken, fastify.verifyUserSession])
         );
     });
 };
--- test/auth/auth.test.ts
+++ test/auth/auth.test.ts
@@ -22,6 +22,7 @@ function generateUser() {
 
 tap.test('Test password hashing', async (test) => {
     const appConfig = getAppConfig();
+    appConfig.USERS_INDEX_NAME = generateModelIndexName();
 
     const app = await buildApp(test, appConfig);
     const user = new UserModel(app, appConfig);
@@ -81,6 +82,8 @@ tap.test('Create user and log in', async (test) => {
             payload: userDraft
         });
 
+        app.log.debug(signUpResp.json());
+
         signUpTest.equal(signUpResp.statusCode, 201, 'Signup response is not 201');
         signUpTest.equal(signUpResp.json().message, 'User created', 'Wrong message');
 
@@ -115,4 +118,70 @@ tap.test('Login with wrong credentials', async (test) => {
     });
 
     test.equal(logInResp.statusCode, 400, 'Auth error response code is not 400');
+});
+
+tap.test('Log in and make sure user getting proper info', async (test) =>  {
+    const appConfig = getAppConfig();
+    appConfig.USERS_INDEX_NAME = generateModelIndexName();
+
+    const app = await buildApp(test, appConfig);
+    const userDraft:UserDraft = generateUser();
+    const userModel = new UserModel(app, appConfig);
+
+    test.before(async () => {
+        await userModel.createIndex();
+    });
+
+    test.teardown(async () => {
+        await userModel.deleteIndex();
+    });
+
+    test.test('Sign up test', async (signUpTest) => {
+        const signUpResp = await app.inject({
+            method: 'POST',
+            url: '/auth/signup',
+            payload: userDraft
+        });
+
+        signUpTest.equal(signUpResp.statusCode, 201, 'Signup response is not 201');
+
+        signUpTest.test('Log in tst', async (logInTest) => {
+            const logInResp = await app.inject({
+                method: 'POST',
+                url: '/auth/login',
+                payload: {
+                    email: userDraft.email,
+                    password: userDraft.password
+                }
+            });
+
+            logInTest.equal(logInResp.statusCode, 200, 'Log in response is not 200');
+
+            let sessionId:string;
+
+            const cookieSession:any = logInResp.cookies[0];
+            if (cookieSession) {
+                sessionId = cookieSession['value'];
+                app.log.debug(`Session: ${sessionId}`);
+            }
+
+            logInTest.test('Getting user data', async (myDataTest) => {
+                const myAccountResp = await app.inject({
+                    method: 'GET',
+                    url: '/profile/',
+                    cookies: {
+                        'session-id': sessionId,
+                    }
+                });
+
+                if (myAccountResp.statusCode !== 200) {
+                    app.log.error(myAccountResp.json());
+                }
+
+                myDataTest.equal(myAccountResp.statusCode, 200, 'Response for profile is not 200');
+                myDataTest.equal(myAccountResp.json().first_name, userDraft.first_name, 'Wrong first name');
+                myDataTest.equal(myAccountResp.json().last_name, userDraft.last_name, 'Wrong first name');
+            });
+        });
+    });
 });
