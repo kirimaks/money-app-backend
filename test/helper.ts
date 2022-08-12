7c656407c19c9bb1a9b0bc7a7494a39e2a5245b3
kirimaks <kirimaks@yahoo.com>
2022-08-12 12:28:40 +0000

    Function to generate user sessions.

--- test/helper.ts
+++ test/helper.ts
@@ -1,14 +1,25 @@
-import Fastify from 'fastify'
-import pino from 'pino';
 import {randomBytes} from 'crypto';
 
+import pino from 'pino';
+import Fastify from 'fastify'
 import fp from 'fastify-plugin';
+import {v4 as uuidv4} from 'uuid';
+
 import {app as App} from '../src/app';
 import {getAppConfig} from '../src/config';
+import {UserModel} from '../src/models/user/user';
 
 import type * as tap from 'tap';
+import type {FastifyInstance} from 'fastify';
 
 export type Test = typeof tap['Test']['prototype'];
+
+
+declare module 'fastify' {
+    interface FastifyInstance {
+        removeAutoIndices: () => void;
+    }
+}
 
 
 async function buildApp (t: Test, config:AppConfig) {
@@ -33,14 +44,78 @@ async function buildApp (t: Test, config:AppConfig) {
     await app.ready();
 
     // Tear down our app after we are done
-    t.teardown(() => void app.close())
+    t.teardown(async () => { 
+        await app.removeAutoIndices();
+        await app.close();
+    });
 
     return app
 }
 
-function generateModelIndexName() {
-    const randomName = randomBytes(8).toString('hex');
+function generateUser() {
+    return {
+        first_name: randomBytes(12).toString('hex'),
+        last_name: randomBytes(12).toString('hex'),
+        phone_number: randomBytes(5).toString('hex'),
+        email: randomBytes(10).toString('hex'),
+        password: randomBytes(12).toString('hex'),
+        account_id: uuidv4(),
+        comment: randomBytes(12).toString('hex'),
+    }
+}
+
+function generateModelIndexName(bytesLength:number) {
+    const randomName = randomBytes(bytesLength).toString('hex');
     return `auto-index-${randomName}`;
 }
 
-export {buildApp, getAppConfig, generateModelIndexName}
+async function generateSession(app:FastifyInstance, appConfig:AppConfig): Promise<string> {
+    const userDraft:UserDraft = generateUser();
+    const userModel = new UserModel(app, appConfig);
+
+    await userModel.createIndex();
+
+    const signUpResp = await app.inject({
+        method: 'POST',
+        url: '/auth/signup',
+        payload: userDraft
+    });
+
+    app.log.debug(`<<< Generate session sing up resp: ${signUpResp.statusCode} >>>`);
+
+    const logInResponse = await app.inject({
+        method: 'POST',
+        url: '/auth/login',
+        payload: {
+            email: userDraft.email,
+            password: userDraft.password,
+        }
+    });
+
+    app.log.debug(`<<< Generate session log in resp: ${logInResponse.statusCode} >>>`);
+
+    const sessionCookie:Cookie|undefined = (logInResponse.cookies as Cookie[]).find((cookie) => {
+        return cookie.name === 'session-id';
+    });
+
+    if (sessionCookie) {
+        app.log.debug(`Session cookie: ${sessionCookie.value}`);
+        return sessionCookie.value;
+
+    } else {
+        app.log.error('Cannot generate session, no cookie returned from login.');
+    }
+
+    return '';
+}
+
+function getTestAppConfig() {
+    const appConfig = getAppConfig();
+
+    appConfig.ACCOUNTS_INDEX_NAME = generateModelIndexName(16);
+    appConfig.USERS_INDEX_NAME = generateModelIndexName(16);
+
+    return appConfig;
+}
+
+export {buildApp, getAppConfig, generateModelIndexName, getTestAppConfig, generateSession}
