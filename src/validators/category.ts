d220a0367c6796d055a0ba3c293620e1342122d4
kirimaks <kirimaks@yahoo.com>
2022-10-27 12:40:42 +0000

    Removed elasticsearch.

--- src/validators/category.ts
+++
@@ -1,7 +0,0 @@
-import type {FastifyInstance, FastifyReply} from 'fastify';
-
-
-export function createCategoryRequestValidator(_fastify:FastifyInstance) {
-    return async (_request:CreateCategoryRequest, _prely:FastifyReply) => {
-    }
-}
--- test/tools.ts
+++
@@ -1,9 +0,0 @@
-import {randomBytes} from 'crypto';
-
-
-function getRandomString(length:number):string {
-    return randomBytes(length/2).toString('hex');
-}
-
-
-export {getRandomString}
