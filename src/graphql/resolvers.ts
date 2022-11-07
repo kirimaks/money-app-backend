7fc3aca83105643bc37f8ea30530dc4b95c5c2d7
kirimaks <kirimaks@yahoo.com>
2022-11-07 15:30:03 +0000

    GraphQL sign up resolver.

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,31 +1,34 @@
 import type { IResolvers } from 'mercurius';
 
 
-// TODO: name, surname,
-type CreateUserPayload = {
-    email: string;
-    password: string;
-};
-
-type LogInPayload = {
-    email: string;
-    password: string;
-};
+// type CreateUserPayload = {
+//     email: string;
+//     password: string;
+//     firstName: string;
+//     lastName: string;
+// };
+// 
+// type LogInPayload = {
+//     email: string;
+//     password: string;
+// };
 
 const GraphQLResolvers:IResolvers = {
     Query: {
-        test(_root, {hello}) {
+        test(obj, payload, context) {
+            context.app.log.debug(`Obj: ${JSON.stringify(obj)}`);
             return 'hello';
         }
     },
     Mutation: {
-        login(_root, {email, password}:LogInPayload) {
-            console.log(`Login: ${email}, ${password}`);
-
+        login(obj, payload, context) {
+            context.app.log.debug(`Obj: ${JSON.stringify(obj)}`);
+            context.app.log.debug(`Login: ${JSON.stringify(payload)}`);
             return 'token';
         },
-        createUser(_root, {email}:CreateUserPayload) {
-            console.log(`Create user: ${email}`);
+        createUser(obj, payload, context) {
+            context.app.log.debug(`Obj: ${JSON.stringify(obj)}`);
+            context.app.log.debug(`New user: ${JSON.stringify(payload)}`);
             return 'done';
         }
     }
