53ebe770c926a5bb80c0f93702c44ae4c5d858fc
kirimaks <kirimaks@yahoo.com>
2022-11-18 10:41:34 +0000

    Auth errors module.

---
+++ src/auth/auth.errors.ts
@@ -0,0 +1,39 @@
+export class EmailExistsError extends Error { 
+    message: string;
+
+    constructor(message:string) {
+        super(message);
+        this.message = message;
+        this.name = 'EmailExistsError';
+    }
+}
+
+export class AuthError extends Error {
+    message: string;
+    
+    constructor(message:string) {
+        super(message);
+        this.message = message;
+        this.name = 'AuthError';
+    }
+}
+
+export class PasswordAuthError extends Error {
+    message: string;
+    
+    constructor(message:string) {
+        super(message);
+        this.message = message;
+        this.name = 'PasswordAuthError';
+    }
+}
+
+export class EmailAuthError extends Error {
+    message: string;
+
+    constructor(message:string) {
+        super(message);
+        this.message = message;
+        this.name = 'EmailAuthError';
+    }
+}
