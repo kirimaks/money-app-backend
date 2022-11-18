b01ca02d3f9bd01d9614b6373ff28c43c3a9e536
kirimaks <kirimaks@yahoo.com>
2022-11-18 10:41:49 +0000

    Auth constatns module.

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,10 @@
+export const SIGN_UP_URL = '/auth/sign-up';
+export const SIGN_IN_URL = '/auth/sign-in';
+
+export const EMAIL_EXISTS_ERROR = 'This email already exists';
+export const INTERNAL_SERVER_ERROR = 'Internal server error, please try again later';
+export const SIGN_IN_PASSWORD_ERROR = 'Authorization error: wrong password';
+export const SIGN_IN_EMAIL_ERROR = 'Authorization error: wrong email';
+
+export const SIGN_UP_OK_MESSAGE = 'User created';
+export const SIGN_IN_OK_MESSAGE = 'Authorized';
