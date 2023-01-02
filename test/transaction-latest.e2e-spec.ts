56d9e3dae1b71eb126184e4816733dde9e50cb2d
kirimaks <kirimaks@yahoo.com>
2023-01-02 05:13:00 +0000

    Latest transactions multiple pages test

--- test/transaction-latest.e2e-spec.ts
+++ test/transaction-latest.e2e-spec.ts
@@ -67,9 +67,9 @@ describe('Latest transactions test', () => {
     await signUpTool(app, testEmail, testPassword);
     const jwtToken = await signInTool(app, testEmail, testPassword);
 
-    const twoDaysAgo = dayjs('2000-01-01').utc().subtract(2, 'days').format();
-    const oneDayAgo = dayjs('2000-01-02').utc().subtract(1, 'days').format();
-    const today = dayjs('2000-01-03').utc().format();
+    const twoDaysAgo = dayjs('2000-01-05').utc().subtract(2, 'days').format();
+    const oneDayAgo = dayjs('2000-01-05').utc().subtract(1, 'days').format();
+    const today = dayjs('2000-01-05').utc().format();
 
     await createTransaction(app, jwtToken, '2daysAgo1', twoDaysAgo, 100);
     await createTransaction(app, jwtToken, '2daysAgo2', twoDaysAgo, 150);
@@ -104,5 +104,80 @@ describe('Latest transactions test', () => {
 
       expect(data?.latestTransactions[1]?.transactions).toHaveLength(2);
       expect(data?.latestTransactions[1]?.totalAmount).toEqual(800);
+    });
+
+    test('Latest transactions pagination', async () => {
+      const testEmail = getRandomEmail();
+      const testPassword = getRandomPassword();
+      await signUpTool(app, testEmail, testPassword);
+      const jwtToken = await signInTool(app, testEmail, testPassword);
+
+      const testDate = '2020-01-20';
+      const threeDaysAgo = dayjs(testDate).utc().subtract(3, 'days').format();
+      const twoDaysAgo = dayjs(testDate).utc().subtract(2, 'days').format();
+      const oneDayAgo = dayjs(testDate).utc().subtract(1, 'days').format();
+      const today = dayjs(testDate).utc().format();
+
+      await createTransaction(app, jwtToken, '3daysAgo1', threeDaysAgo, 100);
+      await createTransaction(app, jwtToken, '3daysAgo2', threeDaysAgo, 200);
+      await createTransaction(app, jwtToken, '3daysAgo3', threeDaysAgo, 300);
+
+      await createTransaction(app, jwtToken, '2daysAgo1', twoDaysAgo, 500);
+      await createTransaction(app, jwtToken, '2daysAgo2', twoDaysAgo, 600);
+
+      await createTransaction(app, jwtToken, '1dayAgo1', oneDayAgo, 50);
+      await createTransaction(app, jwtToken, '1dayAgo2', oneDayAgo, 150);
+      await createTransaction(app, jwtToken, '1dayAgo3', oneDayAgo, 300);
+
+      await createTransaction(app, jwtToken, 'today1', today, 300);
+      await createTransaction(app, jwtToken, 'today2', today, 500);
+
+      const latestTransactionsQuery = gql`
+        query {
+          latestTransactions(timeRangeStart: "${threeDaysAgo}" timeRangeEnd: "${today}") {
+            date totalAmount
+            transactions {
+              datetime amount
+            }
+          }
+        }
+      `;
+
+      const { data } = await request<{ latestTransactions: LatestTransactionsByDay[] }>(app.getHttpServer())
+        .query(latestTransactionsQuery)
+        .set(...getAuthHeader(jwtToken))
+        .expectNoErrors();
+
+      expect(data?.latestTransactions).toHaveLength(2);
+
+      expect(data?.latestTransactions[0]?.transactions).toHaveLength(2);
+      expect(data?.latestTransactions[0]?.totalAmount).toEqual(800);
+
+      expect(data?.latestTransactions[1]?.transactions).toHaveLength(3);
+      expect(data?.latestTransactions[1]?.totalAmount).toEqual(500);
+
+      const latestTransactionsSecondPageQuery = gql`
+        query {
+          latestTransactions(timeRangeStart: "${threeDaysAgo}" timeRangeEnd: "${twoDaysAgo}") {
+            date totalAmount
+            transactions {
+              datetime amount
+            }
+          }
+        }
+      `;
+
+      const secondPageResp = await request<{ latestTransactions: LatestTransactionsByDay[] }>(app.getHttpServer())
+        .query(latestTransactionsSecondPageQuery)
+        .set(...getAuthHeader(jwtToken))
+        .expectNoErrors();
+
+      expect(secondPageResp.data?.latestTransactions).toHaveLength(2);
+
+      expect(secondPageResp.data?.latestTransactions[0]?.transactions).toHaveLength(2);
+      expect(secondPageResp.data?.latestTransactions[0]?.totalAmount).toEqual(1100);
+
+      expect(secondPageResp.data?.latestTransactions[1]?.transactions).toHaveLength(3);
+      expect(secondPageResp.data?.latestTransactions[1]?.totalAmount).toEqual(600);
     });
 });
