50168cd9d4e6cdbbf1aa802b71177e3f479374c0
kirimaks <kirimaks@yahoo.com>
2023-01-08 15:28:10 +0000

    Reset migrations.

--- prisma/migrations/20230108152733_init/migration.sql
+++ prisma/migrations/20230108152733_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL DEFAULT '',
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
@@ -14,33 +15,9 @@ CREATE TABLE "User" (
     "firstName" TEXT NOT NULL DEFAULT '',
     "lastName" TEXT NOT NULL DEFAULT '',
     "accountId" TEXT NOT NULL,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Category" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "accountId" TEXT NOT NULL,
-
-    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Budget" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Budget_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "MoneySource" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "MoneySource_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -48,12 +25,9 @@ CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "amount_cents" BIGINT NOT NULL,
-    "utc_timestamp" TIMESTAMP(3) NOT NULL,
+    "utc_datetime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "accountId" TEXT NOT NULL,
-    "categoryId" TEXT,
     "userId" TEXT,
-    "budgetId" TEXT,
-    "sourceId" TEXT,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
@@ -87,19 +61,7 @@ CREATE TABLE "TransactionTags" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE INDEX "Category_accountId_idx" ON "Category"("accountId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Category_accountId_id_key" ON "Category"("accountId", "id");
-
--- CreateIndex
-CREATE INDEX "Transaction_accountId_categoryId_utc_timestamp_idx" ON "Transaction"("accountId", "categoryId", "utc_timestamp");
-
--- CreateIndex
-CREATE INDEX "Transaction_accountId_utc_timestamp_idx" ON "Transaction"("accountId", "utc_timestamp");
-
--- CreateIndex
-CREATE INDEX "Transaction_accountId_budgetId_utc_timestamp_idx" ON "Transaction"("accountId", "budgetId", "utc_timestamp");
+CREATE INDEX "Transaction_accountId_utc_datetime_idx" ON "Transaction"("accountId", "utc_datetime");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Transaction_accountId_id_key" ON "Transaction"("accountId", "id");
@@ -117,22 +79,10 @@ CREATE UNIQUE INDEX "TransactionTags_transactionId_tagId_key" ON "TransactionTag
 ALTER TABLE "User" ADD CONSTRAINT "User_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Category" ADD CONSTRAINT "Category_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "Budget"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_sourceId_fkey" FOREIGN KEY ("sourceId") REFERENCES "MoneySource"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TagGroup" ADD CONSTRAINT "TagGroup_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
