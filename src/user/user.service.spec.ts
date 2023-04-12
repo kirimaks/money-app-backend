f0bc7ec763dceb02b932704c41e30ecb9a346659
kirimaks <kirimaks@yahoo.com>
2023-04-12 11:37:40 +0000

    Fixed broken test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaClientService } from '../prisma-client/prisma-client.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, PrismaClientService],
     }).compile();
 
     service = module.get<UserService>(UserService);
